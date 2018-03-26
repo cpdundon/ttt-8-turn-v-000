a0f5b1b8d767b5a678fc1a3ac31552a3c8c3f152
Chris Dundon <chris.dundon@gmail.com>
2018-03-26 02:42:50 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,52 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, idx)
+  if idx < 0 || idx > 8
+    return false
+  end
+  
+  !position_taken?(board, idx)
+end
+
+def position_taken?(board, idx)
+  chr = board[idx]
+  if chr == " "
+    return false
+  end
+  true
+end
+
+def move(board, idx, token = "X")
+  
+  if valid_move?(board, idx)
+    board[idx] = token
+  else 
+    return false
+  end
+  
+  display_board(board)
+end
+
+def input_to_index(input)
+  return (input.to_i - 1)
+end
+
+def turn(board)
+  
+  validMove = false
+  tIn = Time.now.to_i
+  
+  until validMove do 
+    puts "Please enter 1-9:"
+    rtn = gets
+    idx = input_to_index(rtn)
+  
+    validMove = move(board, idx)
+  end
+end
